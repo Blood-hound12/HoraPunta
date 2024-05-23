f7b7669f7c29f5e62b215083db2ecf5b57fc882e
Blood-hound12 <h.riverosrobar@uandresbello.edu>
2024-05-23 17:36:09 -0400

    CUASU

--- ¡Hora Punta!/Assets/Scripts/Mechanics/SwipeController.cs
+++ ¡Hora Punta!/Assets/Scripts/Mechanics/SwipeController.cs
@@ -4,41 +4,6 @@ using UnityEngine;
 
 public class SwipeController : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private Vector2 startPos;
-    private bool isSwiping = false;
-
-    public float swipeForce = 10f;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            if (GetComponent<Collider2D>().OverlapPoint(touchPos))
-            {
-                startPos = touchPos;
-                isSwiping = true;
-            }
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (isSwiping)
-            {
-                Vector2 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Vector2 swipeDirection = (endPos - startPos).normalized;
-                rb.AddForce(swipeDirection * swipeForce, ForceMode2D.Impulse);
-                isSwiping = false;
-            }
-        }
-    }
-
     //private Rigidbody2D rb;
     //private Vector2 startPos;
     //private bool isSwiping = false;
@@ -52,26 +17,61 @@ public class SwipeController : MonoBehaviour
 
     //private void Update()
     //{
-    //    if (Input.touchCount > 0)
+    //    if (Input.GetMouseButtonDown(0))
     //    {
-    //        Touch touch = Input.GetTouch(0);
-
-    //        if (touch.phase == TouchPhase.Began)
+    //        Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        if (GetComponent<Collider2D>().OverlapPoint(touchPos))
     //        {
-    //            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-    //            if (GetComponent<Collider2D>().OverlapPoint(touchPos))
-    //            {
-    //                startPos = touchPos;
-    //                isSwiping = true;
-    //            }
+    //            startPos = touchPos;
+    //            isSwiping = true;
     //        }
-    //        else if (touch.phase == TouchPhase.Ended && isSwiping)
+    //    }
+
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        if (isSwiping)
     //        {
-    //            Vector2 endPos = Camera.main.ScreenToWorldPoint(touch.position);
+    //            Vector2 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     //            Vector2 swipeDirection = (endPos - startPos).normalized;
     //            rb.AddForce(swipeDirection * swipeForce, ForceMode2D.Impulse);
     //            isSwiping = false;
     //        }
     //    }
     //}
+
+    private Rigidbody2D rb;
+    private Vector2 startPos;
+    private bool isSwiping = false;
+
+    public float swipeForce = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+           Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+                if (GetComponent<Collider2D>().OverlapPoint(touchPos))
+               {
+                   startPos = touchPos;
+                    isSwiping = true;
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended && isSwiping)
+            {
+                Vector2 endPos = Camera.main.ScreenToWorldPoint(touch.position);
+                Vector2 swipeDirection = (endPos - startPos).normalized;
+                rb.AddForce(swipeDirection * swipeForce, ForceMode2D.Impulse);
+                isSwiping = false;
+            }
+        }
+    }
 }
